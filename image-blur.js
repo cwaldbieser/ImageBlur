810530eb8d56d1ed0c02e0c9a5dcb6670b594346
Carl <cwald@ptd.net>
2011-06-16 23:56:15 -0400

    Made image bluring code object-oriented.

--- image-blur.js
+++ image-blur.js
@@ -1,65 +1,119 @@
 
+var blur_obj = null;
+
 $(document).ready(function(){
+	blur_obj = ImageBlur.init("id_image");
 	$("#id_demo").click(run_demo);	
 })
 ;
 
-/*
 function run_demo()
 {
-	var img = $("#id_image")[0];
-	Pixastic.process(img, "blurfast", {amount:5});
-}
-*/
-
-function run_demo()
-{
-	blur_stages();
-}
-
-function blur_stages()
-{
-	var interval = 75;
-	var apex_interval = 1500;
-	
-	blur_stage(0.1);
-	
-	window.setTimeout(function(){
-		blur_stage(0.5);
+	blur_obj.blurOut(function(){
 		window.setTimeout(function(){
-			blur_stage(1.0);
+			blur_obj.blurIn();
+		}, 1500)
+		;	
+	})
+	;
+}
+
+var ImageBlur = {
+	init: function(image_id, options){
+		var jq_img = $("#" + image_id);
+		var stages = 5;
+		var step = stages / 5.0;
+		var blur_factors = [];
+		for(i=step; i <= 5.0; i += step) blur_factors.push(i);
+		if(blur_factors.length < stages) blur_factors.push(5.0);
+		var ids = [];
+		var img_src = jq_img.attr("src");
+		for(var i=0; i < stages; i++)
+		{
+			var img_id = ImageBlur.uuid();
+			ids.push(img_id);
+			var new_jq_img = $("<img id='" + img_id + "' src='" + img_src + "' />")
+			jq_img.after(new_jq_img);
+			var img = new_jq_img[0];
+			var blur_factor = blur_factors[i];
+			Pixastic.process(img, "blurfast", {amount: blur_factor});
+			$('#' + img_id).hide();
+		}
+		
+		return {
+				'orig_id': image_id,
+				'orig_img': jq_img,
+				'visible_img': jq_img,
+				'delay': 75,
+				'ids': ids,
+				'blurOut': ImageBlur.blur_out,
+				'blurIn': ImageBlur.blur_in,
+				'animate_stages': ImageBlur.animate_stages,
+		}
+		;
+	},	
+	
+	uuid: function()
+	{
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+				var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+				return v.toString(16);
+			})
+			;
+	},
+	
+	blur_out: function(callback)
+	{
+		var stage_count = this.ids.length;
+		var ids = this.ids.slice(0);
+		ids.reverse();
+		var stages = [];
+		for(var i=0; i < ids.length; i++)
+		{
+			var jq_img = $('#' + ids[i]);
+			var delay = this.delay;
+			stages.push([jq_img, delay]);
+		}
+		
+		this.animate_stages(stages, callback);
+	},
+	
+	blur_in: function(callback)
+	{
+		var stage_count = this.ids.length;
+		var ids = [];
+		ids.push(this.orig_id);
+		ids = ids.concat(this.ids.slice(0));
+		var stages = [];
+		for(var i=0; i < ids.length; i++)
+		{
+			var jq_img = $('#' + ids[i]);
+			var delay = this.delay;
+			stages.push([jq_img, delay]);
+		}
+		
+		this.animate_stages(stages, callback);
+	},
+	
+	animate_stages: function(stages, callback)
+	{
+		if(stages.length > 0)
+		{
+			this.visible_img.hide();
+			var item = stages.pop();
+			var jq_img = item[0];
+			var delay = item[1];
+			jq_img.show();
+			this.visible_img = jq_img;
+			var self = this;
 			window.setTimeout(function(){
-				blur_stage(2.5);
-				window.setTimeout(function(){
-					blur_stage(3.75);	
-					window.setTimeout(function(){
-						blur_stage(5.0); //apex
-						window.setTimeout(function(){
-							blur_stage(3.75);
-							window.setTimeout(function(){
-								blur_stage(2.5);
-								window.setTimeout(function(){
-									blur_stage(1.0);
-									window.setTimeout(reset_image, interval);
-								},interval);
-							},interval);
-						},interval);
-					}, apex_interval);
-				},interval);
-			},interval);
-		}, interval);	
-	}, interval);
+				self.animate_stages(stages, callback);
+			}, delay)
+			;
+		}
+		else if(callback) callback();
+	},
 }
+;
 
-function blur_stage(amount)
-{
-	reset_image();
-	var img = $("#id_image")[0];
-	Pixastic.process(img, "blurfast", {amount: amount});
-}
 
-function reset_image()
-{
-	var baseimg = $("#id_baseimage")[0];
-	$("#id_image").replaceWith("<img id='id_image' src='" + baseimg.src + "' />");
-}
