88a4ae8294d730dc3933ccea462ca58c0bcbbf4c
Carl <cwald@ptd.net>
2011-06-17 15:03:47 -0400

    Modularized the blur image functionality.  It is almost plugin-like.

--- image-blur.js
+++ image-blur.js
@@ -1,54 +1,90 @@
 
-var blur_obj = null;
+var transition = null;
 
 $(document).ready(function(){
-	blur_obj = ImageBlur.init("id_image");
+	transition = ImageBlur.init("id_photos");
+
 	$("#id_demo").click(run_demo);	
+	$("#id_stop").click(function(){
+		transition.stop();	
+	})
+	;	
 })
 ;
 
 function run_demo()
 {
-	blur_obj.blurOut(function(){
-		window.setTimeout(function(){
-			blur_obj.blurIn();
-		}, 1500)
-		;	
-	})
-	;
+	transition.run();
 }
 
 var ImageBlur = {
-	init: function(image_id, options){
-		var jq_img = $("#" + image_id);
+	init: function(container_id){
 		var stages = 5;
+		
+		//Configure blur factors for each stage.
 		var step = stages / 5.0;
 		var blur_factors = [];
 		for(i=step; i <= 5.0; i += step) blur_factors.push(i);
 		if(blur_factors.length < stages) blur_factors.push(5.0);
-		var ids = [];
-		var img_src = jq_img.attr("src");
-		for(var i=0; i < stages; i++)
-		{
-			var img_id = ImageBlur.uuid();
-			ids.push(img_id);
-			var new_jq_img = $("<img id='" + img_id + "' src='" + img_src + "' />")
-			jq_img.after(new_jq_img);
-			var img = new_jq_img[0];
-			var blur_factor = blur_factors[i];
-			Pixastic.process(img, "blurfast", {amount: blur_factor});
-			$('#' + img_id).hide();
-		}
+		
+		//Sequence for animations.
+		animation_sequence = [];
+		var blur_factor_count = blur_factors.length;
+		
+		//The set of frames.
+		var animations_set = [];
+		
+		//Process each image.
+		$("#" + container_id + " img").each(function(index){
+			//Create blur animations for each image.
+			var transformations = [];
+			var orig_img = $(this);
+			animations_set.push(orig_img);
+			$.each(blur_factors, function(idx, blur_factor){
+				var new_jq_img = orig_img.clone().insertAfter(orig_img);
+				//new_jq_img.attr('id', ImageBlur.uuid());
+				new_jq_img.attr('id', "id-blur-" + index + "-" + idx);
+				var new_img = new_jq_img[0];
+				var new_elm = Pixastic.process(new_img, "blurfast", {'amount': blur_factor});
+				var new_jq_elm = $(new_elm);
+				transformations.push(new_jq_elm);
+				animations_set.push(new_jq_elm);
+			})
+			;
+			//Add the transformations to the animation sequence.
+			//Sequence should go:
+			//most blurry ... blurry ... not blurry ... blurry ... most blurry
+			animation_sequence = animation_sequence.concat(transformations.slice(0).reverse(), [orig_img], transformations);
+		})
+		;
+		
+		//Rotate the initial sequence to the beginning.
+		//Makes calculating indexes simpler.
+		animation_sequence = animation_sequence.slice(blur_factor_count).concat(animation_sequence.slice(0, blur_factor_count))
+		
+		//Adjust the zIndexes.
+		var frame_count = animation_sequence.length;
+		$.each(animation_sequence, function(idx, jq_obj){
+			jq_obj.css("zIndex", frame_count - idx);
+		})
+		;
 		
 		return {
-				'orig_id': image_id,
-				'orig_img': jq_img,
-				'visible_img': jq_img,
-				'delay': 75,
-				'ids': ids,
-				'blurOut': ImageBlur.blur_out,
-				'blurIn': ImageBlur.blur_in,
-				'animate_stages': ImageBlur.animate_stages,
+			'animations': animation_sequence,
+			'animations_set': animations_set,
+			'current_frame': 0,
+			'frame_count': frame_count,
+			'delay': 75,
+			'pause': 1500,
+			'pause_modulo': (blur_factors.length * 2 + 1),
+			'run_flag': false,
+			'run': function(){
+				this.run_flag = true;
+				ImageBlur.animate.apply(this);
+			},
+			'stop':	function(){
+				this.run_flag = false;
+			},
 		}
 		;
 	},	
@@ -62,57 +98,38 @@ var ImageBlur = {
 			;
 	},
 	
-	blur_out: function(callback)
+	animate: function()
 	{
-		var stage_count = this.ids.length;
-		var ids = this.ids.slice(0);
-		ids.reverse();
-		var stages = [];
-		for(var i=0; i < ids.length; i++)
-		{
-			var jq_img = $('#' + ids[i]);
-			var delay = this.delay;
-			stages.push([jq_img, delay]);
-		}
+		if(!this.run_flag) return;
 		
-		this.animate_stages(stages, callback);
-	},
-	
-	blur_in: function(callback)
-	{
-		var stage_count = this.ids.length;
-		var ids = [];
-		ids.push(this.orig_id);
-		ids = ids.concat(this.ids.slice(0));
-		var stages = [];
-		for(var i=0; i < ids.length; i++)
-		{
-			var jq_img = $('#' + ids[i]);
-			var delay = this.delay;
-			stages.push([jq_img, delay]);
-		}
+		//Advance the current frame.
+		var current_frame = this.current_frame;
+		current_frame++;
+		current_frame %= this.frame_count;
+		this.current_frame = current_frame;
 		
-		this.animate_stages(stages, callback);
-	},
-	
-	animate_stages: function(stages, callback)
-	{
-		if(stages.length > 0)
-		{
-			this.visible_img.hide();
-			var item = stages.pop();
-			var jq_img = item[0];
-			var delay = item[1];
-			jq_img.show();
-			this.visible_img = jq_img;
-			var self = this;
-			window.setTimeout(function(){
-				self.animate_stages(stages, callback);
-			}, delay)
-			;
-		}
-		else if(callback) callback();
-	},
+		//Determine the delay time.
+		var delay = this.delay;
+		if(current_frame % this.pause_modulo == 0) delay = this.pause;
+		var self = this;
+		
+		//Rotate the z-indexes.
+		var animations = this.animations;
+		var animations_set = this.animations_set;
+		var frame_count = this.frame_count;
+		$.each(animations_set, function(idx, jq_obj){
+			jq_obj.css("zIndex", 0);
+		})
+		;
+		animations[current_frame].css("zIndex", 2);
+		animations[(current_frame + 1) % frame_count].css("zIndex", 1);
+		
+		//
+		window.setTimeout(function(){
+			ImageBlur.animate.apply(self);	
+		}, delay)
+		;
+	}
 }
 ;
 
